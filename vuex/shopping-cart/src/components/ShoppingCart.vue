0ba4fa5a9f450029b589d542a9beb336944d9f42
dxanh <xuananh@MBA-Anh.local>
2018-10-23 23:39:23 +0700

    up

---
+++ vuex/shopping-cart/src/components/ShoppingCart.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h1>Shopping Cart</h1>
+        <ul>
+            <li v-for="product in products">
+                {{product.title}} - {{product.price | currency }} - {{product.quantity}}
+
+            </li>
+        </ul>
+        <p>Total: {{total | currency}}</p>
+        
+        <!-- <button @click="$store.dispatch('checkout')">Checkout</button> -->
+        <button @click="checkout">Checkout</button>
+        <!-- <p v-if="$store.state.checkoutStatus">{{$store.state.checkoutStatus}}</p> -->
+        <p v-if="checkoutStatus">{{checkoutStatus}}</p>
+    </div>
+</template>
+
+<script>
+import { mapState, mapGetter, mapActions, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    // products() {
+    //   return this.$store.getters.cartProducts;
+    // },
+
+    // total() {
+    //   return this.$store.getters.cartTotal;
+    // }
+
+    // Using mapGetters
+
+    ...mapGetters('cart', {
+      products: 'cartProducts',
+      total: 'cartTotal'
+    }),
+
+    ...mapState('cart', {
+      // checkoutStatus: "checkoutStatus"
+      // checkoutStatus: state => state.cart.checkoutStatus
+      checkoutStatus: state => state.checkoutStatus
+    })
+  },
+
+  // Add methods and mapAction to replace $store.dispatch
+
+  methods: {
+    // ...mapActions(["checkout"])
+    ...mapActions('cart', ['checkout'])
+  }
+};
+</script>
+
+<style>
+</style>
+
---
+++ vuex/shopping-cart/src/store/actions.js
@@ -0,0 +1,19 @@
+import shop from "@/api/shop";
+
+export default { // = methods
+
+
+
+  // addProductToCart(context, product) {
+  //   if (product.inventory > 0) {
+  //     const cartItem = context.state.cart.find(item => item.id === product.id)
+  //     if (!cartItem) {
+  //       context.commit('pushProductToCart', product.id)
+  //     } else {
+  //       context.commit('incrementItemQuantity', cartItem)
+  //     }
+  //     context.commit('decrementProductInventory', product)
+  //   }
+  // },
+}
+
